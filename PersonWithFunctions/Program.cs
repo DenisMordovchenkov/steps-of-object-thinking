7353719c5a1d974bf4dc487c97b51fb9d5260b8a
DenisMordovchenkov <denisbabichev@gmail.com>
2025-11-02 10:42:28 +0300

    Wrote function to create list with random persons

---
+++ PersonWithFunctions/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+String[] CreatePersons(int amount)
+{
+    String[] names = new String[amount];
+    for (int i = 0; i < amount; i++)
+    {
+        names[i] = $"Человек Человеков - {i}, Возраст - {Random.Shared.Next(99)}";
+    }
+    return names;
+}
+;
+
+
+String[] persons = CreatePersons(10);
+
+foreach (var item in persons)
+{
+    Console.WriteLine(item);
+}
+
+
+
