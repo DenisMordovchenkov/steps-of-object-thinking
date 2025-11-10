c0fdeef5711bb1aa2a4689f082935d8d866926e4
DenisMordovchenkov <denisbabichev@gmail.com>
2025-11-10 09:16:38 +0300

    added two realization of creating Person. With Functions and with Classes

---
+++ PersonsWithClass/Program.cs
@@ -0,0 +1,41 @@
+﻿Person person1 = new("Иван Иванов", 21);
+
+person1.SetAge(120);
+
+Console.WriteLine(person1.GetName());
+Console.WriteLine(person1.GetAge());
+
+class Person
+{
+    private string name;
+    private int age;
+
+    public Person(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+    //Геттеры
+    public string GetName()
+    {
+        return name;
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+    //Сеттеры
+    public void SetName(string n)
+    {
+        name = n;
+    }
+
+    public void SetAge(int a)
+    {
+        age = a;
+    }
+}
+;
+
+
